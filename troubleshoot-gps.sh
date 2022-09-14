77f5fa39900d115ac7009e0dd76ee029cd8d58cf
moose_crewhard_ <15912502+iosecurityio@users.noreply.github.com>
2022-09-14 19:47:10 -0400

    check-gps is now troubleshoot

--- troubleshoot-gps.sh
+++ troubleshoot-gps.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# If there is an output here, you are good to go
 sudo stty -F /dev/ttyUSB0 ispeed 4800 && sudo cat </dev/ttyUSB0
-# if GPS isnt started
-# Ensure Plug in adapter
+
+# Else GPS isnt started
+# Ensure Plugged in adapter
 # sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
-# if GPS is started
+
+# If all else fails:
 # sudo killall gpsd
 # sudo rm /var/run/gpsd.sock
 # Go back to step 1
+
+# cgps / xgps for checks
