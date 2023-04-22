1dd2982e2b30660f374eb090231064be72b14fd2
iosecurity.io <15912502+iosecurityio@users.noreply.github.com>
2023-04-22 15:24:30 -0400

    warwheel update because end of winter

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Warwheel
 
-Warwheel is a project that will essentially allow a user on a PEV to Wardrive.
-In my case, I will share my setup and configuration files that I use in a Sling pack while foating on a Onewheel.
+Warwheel is a project that enables the rider of a Personal Electric Vehicle (PEV) to Wardrive, hence "warwheel"
 
-`chmod +x ./bootstrap_warwheel.sh`
-`./bootstrap_warwheel.sh`
+In my case, I will share my setup and configuration files that I use in a Sling pack while floating on a Onewheel.
+
+> Always wear your helmet! 🪖
+
+## Usage
+
+- `chmod +x ./bootstrap_warwheel.sh`
+- `./bootstrap_warwheel.sh`
---
+++ tests/tests.py
@@ -0,0 +1,32 @@
+# TESTS
+from colorama import init, Fore
+from datetime import datetime, timedelta
+init(autoreset=True) # initialize colarama
+
+class warwheel:
+        """Creates a warwheel object"""
+
+        def __init__(self):
+                self.start_time = ""
+                self.start_date = datetime.now().strftime("%m-%d-%Y")
+                self.start_location = ""
+                self.splash()
+
+        def splash(self):
+                print
+                print("Warwheel v0.0.1")
+                print("Created by: @113n")
+                print("https://github.com/iosecurityio/warwheel")
+                print("*" * 50)
+
+        def start(self):
+                self.start_time = datetime.now().strftime("%H:%M:%S")
+                #self.start_location = input("Where are you starting from? ")
+
+def main():
+      print("Starting warwheel...")
+      warwheeler = warwheel()
+      print(warwheeler.start_date)
+
+if __name__ == '__main__':
+    main()
